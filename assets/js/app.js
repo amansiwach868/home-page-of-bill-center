2a224cf9c80cbef9ecc4af22746d96d78a18ec28
amansiwach868 <amansiwach868@gmail.com>
2025-05-20 16:26:16 +0530

    section-2 stetic complete

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,3 +4,10 @@ document.getElementById("menu-toggle").addEventListener("click", function () {
     document.getElementById("span-2").classList.toggle("activeNavBar3");
     document.getElementById("span-3").classList.toggle("activeNavBar");
 });
+
+$('.autoplay').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+});
