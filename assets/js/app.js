c9361f5487fe37f47320960baba4ccc274086e0a
amansiwach868 <amansiwach868@gmail.com>
2025-05-22 09:18:33 +0530

    There were some syntax errors now fixed

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,4 @@
-document.getElementById("menu-toggle").addEventListener("click", function () {
+document.getElementById("menuToggle").addEventListener("click", function () {
     document.getElementById("menu").classList.toggle("show");
     document.getElementById("span-1").classList.toggle("activeNavBar2");
     document.getElementById("span-2").classList.toggle("activeNavBar3");
